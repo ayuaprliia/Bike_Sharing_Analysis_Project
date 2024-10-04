d28dad1e9d5a5ff0fb44466f04c78494b5d2ad00
Ayu Aprilia <114905113+ayuaprliia@users.noreply.github.com>
2024-10-04 17:18:05 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,8 +7,6 @@ all_df = pd.read_csv("https://raw.githubusercontent.com/ayuaprliia/Bike_Sharing_
 
 # title
 st.title("Dashboard Bike Sharing Dataset:bike:")
-st.sidebar.image("dashboard/logo.jpg")
-
 
 # Home
 def show_home():
